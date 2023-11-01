cbcced11cff8b92a4df95bb9e620f90e1fe45a0a
shahruz2510 <122316831+shahruz2510@users.noreply.github.com>
2023-11-01 13:10:11 +0500

    Changed Motherfucker to Asshole

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Hello,Motherfucker'),
+        child: Text('Hello,Asshole'),
       ),
     );
   }
